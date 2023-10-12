5479c5291bd2a20e030f12298a9a1fe762db81a8
rennebarduco <143908565+rennebarduco@users.noreply.github.com>
2023-10-11 23:30:21 -0300

    parte client
    parte cliente do projeto negociacao.

---
+++ js/app/views/View.js
@@ -0,0 +1,16 @@
+class View {
+    
+    constructor (elemento){
+        this._elemento = elemento;
+    
+    } 
+
+    _template(){
+
+        throw new Error ('O método template deve ser implementado');
+    }
+
+    update (model) {
+        this._elemento.innerHTML = this._template(model);
+    }
+}
